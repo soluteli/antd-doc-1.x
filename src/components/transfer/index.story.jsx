9ba0a9675df0cd30ed3f39cce260f71d37a24df8
soluteli <soluteli@163.com>
2021-04-27 17:04:51 +0800

    feat: add transfer

---
+++ src/components/transfer/index.story.jsx
@@ -0,0 +1,211 @@
+import React from 'react'
+
+import { Transfer, Button } from 'antd';
+
+export const Basic = React.createClass({
+  getInitialState() {
+    return {
+      mockData: [],
+      targetKeys: [],
+    };
+  },
+  componentDidMount() {
+    this.getMock();
+  },
+  getMock() {
+    const targetKeys = [];
+    const mockData = [];
+    for (let i = 0; i < 20; i++) {
+      const data = {
+        key: i,
+        title: `内容${i + 1}`,
+        description: `内容${i + 1}的描述`,
+        chosen: Math.random() * 2 > 1,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({ mockData, targetKeys });
+  },
+  handleChange(targetKeys, direction, moveKeys) {
+    console.log(targetKeys, direction, moveKeys);
+    this.setState({ targetKeys });
+  },
+  render() {
+    return (
+      <Transfer
+        dataSource={this.state.mockData}
+        targetKeys={this.state.targetKeys}
+        onChange={this.handleChange}
+        render={item => item.title}
+      />
+    );
+  },
+});
+
+export const Advanced = React.createClass({
+  getInitialState() {
+    return {
+      mockData: [],
+      targetKeys: [],
+    };
+  },
+  componentDidMount() {
+    this.getMock();
+  },
+  getMock() {
+    const targetKeys = [];
+    const mockData = [];
+    for (let i = 0; i < 20; i++) {
+      const data = {
+        key: i,
+        title: `内容${i + 1}`,
+        description: `内容${i + 1}的描述`,
+        chosen: Math.random() * 2 > 1,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({ mockData, targetKeys });
+  },
+  handleChange(targetKeys) {
+    this.setState({ targetKeys });
+  },
+  renderFooter() {
+    return (
+      <Button type="ghost" size="small" style={{ float: 'right', margin: 5 }}
+        onClick={this.getMock}
+      >
+        刷新
+      </Button>
+    );
+  },
+  render() {
+    return (
+      <Transfer
+        dataSource={this.state.mockData}
+        showSearch
+        listStyle={{
+          width: 250,
+          height: 300,
+        }}
+        operations={['向右操作文案', '向左操作文案']}
+        targetKeys={this.state.targetKeys}
+        onChange={this.handleChange}
+        render={item => `${item.title}-${item.description}`}
+        footer={this.renderFooter}
+      />
+    );
+  },
+});
+
+export const Search = React.createClass({
+  getInitialState() {
+    return {
+      mockData: [],
+      targetKeys: [],
+    };
+  },
+  componentDidMount() {
+    this.getMock();
+  },
+  getMock() {
+    const targetKeys = [];
+    const mockData = [];
+    for (let i = 0; i < 20; i++) {
+      const data = {
+        key: i,
+        title: `内容${i + 1}`,
+        description: `内容${i + 1}的描述`,
+        chosen: Math.random() * 2 > 1,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({ mockData, targetKeys });
+  },
+  filterOption(inputValue, option) {
+    return option.description.indexOf(inputValue) > -1;
+  },
+  handleChange(targetKeys) {
+    this.setState({ targetKeys });
+  },
+  render() {
+    return (
+      <Transfer
+        dataSource={this.state.mockData}
+        showSearch
+        filterOption={this.filterOption}
+        targetKeys={this.state.targetKeys}
+        onChange={this.handleChange}
+        render={item => item.title}
+      />
+    );
+  },
+});
+
+
+export const CustomItem = React.createClass({
+  getInitialState() {
+    return {
+      mockData: [],
+      targetKeys: [],
+    };
+  },
+  componentDidMount() {
+    this.getMock();
+  },
+  getMock() {
+    const targetKeys = [];
+    const mockData = [];
+    for (let i = 0; i < 20; i++) {
+      const data = {
+        key: i,
+        title: `内容${i + 1}`,
+        description: `内容${i + 1}的描述`,
+        chosen: Math.random() * 2 > 1,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({ mockData, targetKeys });
+  },
+  handleChange(targetKeys, direction, moveKeys) {
+    console.log(targetKeys, direction, moveKeys);
+    this.setState({ targetKeys });
+  },
+  renderItem(item) {
+    const customLabel = (
+      <span className="custom-item">
+        {item.title} - {item.description}
+      </span>
+    );
+
+    return {
+      label: customLabel,  // for displayed item
+      value: item.title,   // for title and filter matching
+    };
+  },
+  render() {
+    return (
+      <Transfer
+        dataSource={this.state.mockData}
+        listStyle={{
+          width: 300,
+          height: 300,
+        }}
+        targetKeys={this.state.targetKeys}
+        onChange={this.handleChange}
+        render={this.renderItem}
+      />
+    );
+  },
+});
